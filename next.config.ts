4f1f073d6e1927777ab54a56e9f76f82d6c0fed1
sbb45 <gwerg@gr.com>
2025-05-29 17:53:12 +0500

    agents bugfix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    compiler: {
+        styledComponents: {
+            ssr: true,
+            displayName: true,
+        },
+    },
 };
 
 export default nextConfig;
